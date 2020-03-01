3caf8ebbc48326270768bd0c754a74e5556b7ba6
DreamPiggy <lizhuoli1126@126.com>
2020-03-01 12:43:45 +0800

    Update the documentation, add support to snapshot specify Lottie animation frame bitmap

--- SDWebImageLottiePlugin/Classes/LOTAnimatedImage.h
+++ SDWebImageLottiePlugin/Classes/LOTAnimatedImage.h
@@ -9,8 +9,34 @@
 #import <SDWebImage/SDWebImage.h>
 #import <Lottie/Lottie.h>
 
+/**
+ A wrapper class to allow `LOTComposition` to be compatible for SDWebImage loading/cache/rendering system. The `GIF` image loading from `LOTCompositionView+WebCache` category, will use this subclass instead of `UIImage`.
+
+ @note Though this class conforms to `SDAnimatedImage` protocol, so it's compatible to be used for `SDAnimatedImageView`. But it's normally discouraged to do so. Because it does not provide optimization for animation rendering. Instead, use `SDAnimatedImage` class with `SDAnimatedImageView`.
+ @note If you want to get the bitmap version of Lottie animation at specify frame, call `animatedImageFrameAtIndex:` will result a snapshot for frame.
+*/
 @interface LOTAnimatedImage : UIImage <SDAnimatedImage>
 
+/**
+ The `LOTComposition` instance for Lottie representation.
+*/
 @property (nonatomic, strong, readonly, nullable) LOTComposition *composition;
+
+/**
+ Create the wrapper with specify `LOTComposition` instance. The instance should be nonnull.
+ This is a convenience method for some use cases, for example, create a placeholder with `LOTComposition`.
+ @param composition The `LOTComposition` instance
+ @return An initialized object
+*/
+- (nonnull instancetype)initWithComposition:(nonnull LOTComposition *)composition;
+
+// This class override these methods from UIImage, and it supports NSSecureCoding.
+// You should use these methods to create a new animated image. Use other methods just call super instead.
++ (nullable instancetype)imageWithContentsOfFile:(nonnull NSString *)path;
++ (nullable instancetype)imageWithData:(nonnull NSData *)data;
++ (nullable instancetype)imageWithData:(nonnull NSData *)data scale:(CGFloat)scale;
+- (nullable instancetype)initWithContentsOfFile:(nonnull NSString *)path;
+- (nullable instancetype)initWithData:(nonnull NSData *)data;
+- (nullable instancetype)initWithData:(nonnull NSData *)data scale:(CGFloat)scale;
 
 @end
--- SDWebImageLottiePlugin/Classes/LOTAnimatedImage.m
+++ SDWebImageLottiePlugin/Classes/LOTAnimatedImage.m
@@ -7,14 +7,55 @@
 //
 
 #import "LOTAnimatedImage.h"
+#undef UIColor
+#import "LOTCompositionContainer.h"
 
 @interface LOTAnimatedImage ()
 
 @property (nonatomic, copy, nullable) NSData *animatedImageData;
+@property (nonatomic, strong, nullable) LOTCompositionContainer *compositionContainer;
 
 @end
 
 @implementation LOTAnimatedImage
+
+- (instancetype)initWithComposition:(LOTComposition *)composition {
+#if SD_UIKIT
+    self = [super init];
+#else
+    self = [super initWithSize:composition.compBounds.size];
+#endif
+    if (self) {
+        _composition = composition;
+        _animatedImageData = nil;
+    }
+    return self;
+}
+
++ (instancetype)imageWithContentsOfFile:(NSString *)path {
+    return [[self alloc] initWithContentsOfFile:path];
+}
+
++ (instancetype)imageWithData:(NSData *)data {
+    return [[self alloc] initWithData:data];
+}
+
++ (instancetype)imageWithData:(NSData *)data scale:(CGFloat)scale {
+    return [[self alloc] initWithData:data scale:scale];
+}
+
+- (instancetype)initWithData:(NSData *)data {
+    return [self initWithData:data scale:1];
+}
+
+- (instancetype)initWithContentsOfFile:(NSString *)path {
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    return [self initWithData:data];
+}
+
+- (instancetype)initWithData:(NSData *)data scale:(CGFloat)scale {
+    return [self initWithData:data scale:scale options:nil];
+}
 
 - (instancetype)initWithData:(NSData *)data scale:(CGFloat)scale options:(SDImageCoderOptions *)options {
     NSError *error;
@@ -43,11 +84,8 @@
 }
 
 - (instancetype)initWithAnimatedCoder:(id<SDAnimatedImageCoder>)animatedCoder scale:(CGFloat)scale {
-    NSData *data = animatedCoder.animatedImageData;
-    if (!data) {
-        return nil;
-    }
-    return [self initWithData:data scale:scale options:nil];
+    // Does not support progressive load for Lottie images at all
+    return nil;
 }
 
 #if SD_UIKIT
@@ -60,10 +98,73 @@
 }
 #endif
 
+#pragma mark - NSSecureCoding
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        NSData *animatedImageData = [aDecoder decodeObjectOfClass:[NSData class] forKey:NSStringFromSelector(@selector(animatedImageData))];
+        if (!animatedImageData) {
+            return self;
+        }
+        NSError *error;
+        NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:animatedImageData options:0 error:&error];
+        if (error) {
+            NSLog(@"%@", error);
+            return nil;
+        }
+        LOTComposition *composition = [LOTComposition animationFromJSON:jsonObject];
+        if (!composition) {
+            return nil;
+        }
+        _composition = composition;
+        _animatedImageData = animatedImageData;
+#if SD_MAC
+        self.size = composition.compBounds.size;
+#endif
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [super encodeWithCoder:aCoder];
+    NSData *animatedImageData = self.animatedImageData;
+    if (animatedImageData) {
+        [aCoder encodeObject:animatedImageData forKey:NSStringFromSelector(@selector(animatedImageData))];
+    }
+}
+
+#pragma mark - Helper
+
+- (LOTCompositionContainer *)compositionContainer {
+    if (!_compositionContainer) {
+        _compositionContainer = [[LOTCompositionContainer alloc] initWithModel:nil inLayerGroup:nil withLayerGroup:self.composition.layerGroup withAssestGroup:self.composition.assetGroup];
+    }
+    return _compositionContainer;
+}
+
+- (UIImage *)createFrameWithComposition:(nonnull LOTComposition *)composition frameIndex:(NSUInteger)frameIndex {
+    LOTCompositionContainer *container = self.compositionContainer;
+    if (!container) {
+        return nil;
+    }
+    [container displayWithFrame:@(frameIndex) forceUpdate:YES];
+    SDGraphicsImageRenderer *renderer = [[SDGraphicsImageRenderer alloc] initWithSize:composition.compBounds.size];
+    UIImage *image = [renderer imageWithActions:^(CGContextRef  _Nonnull context) {
+        [container renderInContext:context];
+    }];
+    return image;
+}
+
 #pragma mark - SDAnimatedImage
 
 - (UIImage *)animatedImageFrameAtIndex:(NSUInteger)index {
-    return nil;
+    LOTComposition *composition = self.composition;
+    if (!composition) {
+        return nil;
+    }
+    UIImage *frame = [self createFrameWithComposition:composition frameIndex:index];
+    return frame;
 }
 
 - (NSTimeInterval)animatedImageDurationAtIndex:(NSUInteger)index {
@@ -71,7 +172,11 @@
     if (!composition) {
         return 0;
     }
-    return composition.timeDuration;
+    NSUInteger frameCount = self.animatedImageFrameCount;
+    if (frameCount == 0) {
+        return 0;
+    }
+    return composition.timeDuration / frameCount;
 }
 
 - (NSUInteger)animatedImageLoopCount {
@@ -83,7 +188,7 @@
     if (!composition) {
         return 0;
     }
-    return (composition.endFrame.doubleValue - composition.startFrame.doubleValue) * composition.framerate.doubleValue;
+    return (composition.endFrame.unsignedIntegerValue - composition.startFrame.unsignedIntegerValue);
 }
 
 @end
